cc42c16b212299a747404cae5a4385da9aaa5fad
Juliomdz <julio.mdz@hotmail.com>
2024-07-01 11:22:57 -0300

    corrección Pipe turnos

--- src/app/pipes/horario-turno.pipe.ts
+++ src/app/pipes/horario-turno.pipe.ts
@@ -5,19 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HorarioTurnoPipe implements PipeTransform {
 
+  private meses: string[] = [
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+    'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+  ];
+
   transform(value: any, ...args: unknown[]): unknown {
 
     if (value.seconds) {
       value = new Date(value.seconds * 1000);
     }
 
-    let rtn = ''
+    let rtn = '';
     const dia = value.getDate().toString().padStart(2, '0');
-    const mes = (value.getMonth() + 1).toString().padStart(2, '0');
-    const hora = value.getHours() + 2
+    const mes = this.meses[value.getMonth()];
+    const hora = (value.getHours() + 2).toString().padStart(2, '0');
     const minutos = value.getMinutes().toString().padStart(2, '0');
 
-    rtn =`${mes}/${dia} ${hora}:${minutos}`
+    rtn = `${dia} de ${mes} ${hora}:${minutos}`;
 
     return rtn;
   }
